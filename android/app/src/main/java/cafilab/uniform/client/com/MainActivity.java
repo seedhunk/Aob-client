d8136ef003bda2f5fda4f20b7947c134491edf70
seedhunk <89885767+seedhunk@users.noreply.github.com>
2024-08-29 18:44:05 +0800

    Add files via upload

---
+++ android/app/src/main/java/cafilab/uniform/client/com/MainActivity.java
@@ -0,0 +1,5 @@
+package cafilab.uniform.client.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'aob.uniform.cafi.com',
+  appName: 'AoB Uniform',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
